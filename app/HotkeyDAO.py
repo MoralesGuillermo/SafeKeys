93c787278deca178ef0a08abe7d67c218e38add1
Guillermo Morales <morales.guillermo2503@gmail.com>
2023-09-13 20:19:11 -0600

    Implemented DAO for model management

---
+++ app/HotkeyDAO.py
@@ -0,0 +1,44 @@
+""""DAO object to retrieve data from hk.py"""
+import filemanager as fm
+
+
+class HotkeyDAO:
+    def __init__(self, filepath):
+        self._filepath = filepath
+        self._raw_file = str
+        self._key = None
+
+    @property
+    def filepath(self) -> str:
+        """self.filepath attr getter"""
+        return self._filepath
+
+    @filepath.setter
+    def filepath(self, path) -> None:
+        """Set self.filepath"""
+        self._filepath = path
+
+    @property
+    def key(self) -> int:
+        """self.filepath attr getter"""
+        return self._key
+
+    @key.setter
+    def key(self, key) -> None:
+        """Set self.filepath"""
+        self._key = key
+
+    def read_file(self) -> None:
+        """Read the given hotkeyfile. """
+        self._raw_file = fm.FileManager.read_content(self.filepath)
+        if self._raw_file is None:
+            print(f"GIVEN FILE {self.filepath} has not been found: DAO")
+        else:
+            self.key = sum(map(lambda x: int(x), self._raw_file.split(",")))
+
+    def write_file(self, content) -> None:
+        """Write the hotkey file"""
+        fm.FileManager.write_file(self.filepath, content)
+
+
+
